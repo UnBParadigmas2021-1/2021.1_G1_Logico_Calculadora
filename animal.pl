dcd6336f56a8615a9af030cf3e18bd77bcd23c96
Wictorgirardi <wictor.girardi@gmail.com>
2021-09-13 16:25:25 -0300

    novo animal

--- animal.pl
+++ animal.pl
@@ -1,15 +1,16 @@
 /* Bem vindo ao mini jogo de descubra o animal! Para começar digite .init  */
 
 init :- chute(Animal),
+      write('** Bem vindo ao jogo do bicho! Iremos fazer algumas perguntas e tentar adivinhar qual animal você está pensando! **'), nl, nl,
       write('Eu acho que o seu animal é um: '),
       write(Animal),
       nl,
       undo.
 
 /* Animais */
-
 chute(capivara)   :- capivara, !.
 chute(macaco)     :- macaco, !.
+chute(jacare)     :- jacare, !.
 chute(desconhecido).  /* não descobrimos seu animal */
 
 /* animais e suas caracteristicas  */
@@ -22,10 +23,17 @@ macaco :- mamifero,
          herbivoro,
          verify(ele_vive_em_arvores), 
          verify(pode_ser_domesticado).
+jacare :- reptil,  
+         carnivoro,
+         verify(ele_nada), 
+         verify(voce_se_torna_um_ao_ser_vacinado),
+         verify(seu_apelido_popular_é_jaré).
 
 /* classificacoes */
 mamifero    :- verify(tem_pelo), !.
 mamifero    :- verify(produz_leite).
+reptil    :- verify(possui_pele_grossa), !.
+reptil    :- verify(sao_conhecidos_por_serem_animais_pre_historicos).
 ave      :- verify(possui_penas), !.
 ave      :- verify(voa), 
              verify(bota_ovos).
